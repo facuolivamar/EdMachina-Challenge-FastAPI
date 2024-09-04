338aaf10845d831a1618687bb409dab3eb3bc737
facuolivamar <facundoolivam@gmail.com>
2024-09-04 15:10:30 -0300

    selects sql

---
+++ postgresql_scripts/selects.sql
@@ -0,0 +1,10 @@
+
+SELECT * FROM carreras;
+SELECT * FROM personas;
+SELECT * FROM materias;
+SELECT * FROM registros;
+
+SELECT * FROM carreras WHERE id = 1;
+SELECT * FROM personas WHERE nombre_persona = 'Juan';
+SELECT * FROM materias WHERE anio_materia = 2024;
+SELECT * FROM registros WHERE activo = true;
