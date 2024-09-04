460ec48a3e165f70e55bb4710e8270024a4b4ca2
facuolivamar <facundoolivam@gmail.com>
2024-09-04 01:02:29 -0300

    Registros endpoint: update_registro

--- app/routers/registros.py
+++ app/routers/registros.py
@@ -46,3 +46,19 @@ async def create_registro(db: db_dependency, registro_request: RegistroRequest):
 
     db.add(registro_model)
     db.commit()
+
+@router.put("/{registro_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_registro(db: db_dependency, registro_request: RegistroRequest, registro_id: int = Path(gt=0)):
+    registro_model = db.query(Registros).filter(Registros.id == registro_id).first()
+    if registro_model is None:
+        raise HTTPException(status_code=404, detail='Registro not found.')
+
+    registro_model.calificacion_final = registro_request.calificacion_final
+    registro_model.fecha_inicio_cursado = registro_request.fecha_inicio_cursado
+    registro_model.fecha_fin_cursado = registro_request.fecha_fin_cursado
+    registro_model.activo = registro_request.activo
+    registro_model.persona_id = registro_request.persona_id
+    registro_model.materia_id = registro_request.materia_id
+
+    db.add(registro_model)
+    db.commit()
