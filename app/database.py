cf0902a9417e1697fa022eca87ce2533afa5c811
facuolivamar <facundoolivam@gmail.com>
2024-09-04 09:03:06 -0300

    environment variable for database

--- app/database.py
+++ app/database.py
@@ -1,8 +1,14 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./app/leadsapp.db'
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')
+
+print("DATABASE_URL:", SQLALCHEMY_DATABASE_URL)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL,
                        connect_args={'check_same_thread': False})
