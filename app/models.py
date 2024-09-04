cf28a72d01c5f37ce167b57d8d44fffa6f099143
facuolivamar <facundoolivam@gmail.com>
2024-09-04 00:10:01 -0300

    Personas model orm

--- app/models.py
+++ app/models.py
@@ -7,3 +7,13 @@ class Carreras(Base):
     
     id = Column(Integer, primary_key=True, index=True)
     nombre_carrera = Column(String(length=255))
+
+class Personas(Base):
+    __tablename__ = 'personas'
+
+    id = Column(Integer, primary_key=True, index=True)
+    nombre_persona = Column(String(length=255))
+    apellido_persona = Column(String(length=255))
+    email_persona = Column(String, unique=True) 
+    numero_dni_persona = Column(Integer)
+    anio_nacimiento_persona = Column(Date)
