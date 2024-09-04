156846e8de254dbd093e7c39a7953c823f29e2f1
facuolivamar <facundoolivam@gmail.com>
2024-09-04 00:28:59 -0300

    materia endpoint: update_materia

--- app/routers/materias.py
+++ app/routers/materias.py
@@ -45,3 +45,17 @@ async def create_materia(db: db_dependency, materia_request: MateriaRequest):
     db.add(materia_model)
     db.commit()
 
+
+@router.put("/{materia_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_materia(db: db_dependency, materia_request: MateriaRequest, materia_id: int = Path(gt=0)):
+    materia_model = db.query(Materias).filter(Materias.id == materia_id).first()
+    if materia_model is None:
+        raise HTTPException(status_code=404, detail='Materia not found.')
+
+    materia_model.nombre_materia = materia_request.nombre_materia
+    materia_model.anio_materia = materia_request.anio_materia
+    materia_model.carrera_id = materia_request.carrera_id
+
+
+    db.add(materia_model)
+    db.commit()
