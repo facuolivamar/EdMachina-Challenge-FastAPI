c0d68ddddde003cc6d1e4a4f97eac7b4b35e87dd
facuolivamar <facundoolivam@gmail.com>
2024-09-04 22:12:25 -0300

    db_diagram and inserts.sql updated

--- postgresql_scripts/inserts.sql
+++ postgresql_scripts/inserts.sql
@@ -1,7 +1,7 @@
 INSERT INTO carreras (nombre_carrera)
 VALUES ('Ingeniería en Sistemas');
 
-INSERT INTO personas (nombre_persona, apellido_persona, email_persona, numero_dni_persona, anio_nacimiento_persona)
+INSERT INTO personas (nombre_persona, apellido_persona, email_persona, numero_dni_persona, fecha_nacimiento_persona)
 VALUES ('Juan', 'Pérez', 'juan.perez@example.com', 12345678, '1980-05-15');
 
 INSERT INTO materias (nombre_materia, anio_materia, carrera_id)
