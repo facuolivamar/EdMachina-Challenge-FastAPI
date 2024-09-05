328c8c6300599ed7e3701d2aa20218e1b9e5b036
facuolivamar <facundoolivam@gmail.com>
2024-09-05 01:10:50 -0300

    Comments in code #2

--- app/routers/registros.py
+++ app/routers/registros.py
@@ -9,12 +9,14 @@ from ..models.materias import Materias
 from ..models.personas import Personas
 from ..schemas.registros import RegistroRequest
 
+# Definimos un router para el manejo de las rutas relacionadas con los "registros"
 router = APIRouter(
     prefix='/registro',
     tags=['registro']
 )
 
 
+# Función para obtener una sesión de base de datos"
 def get_db():
     db = SessionLocal()
     try:
@@ -23,55 +25,72 @@ def get_db():
         db.close()
 
 
+# Definición de la dependencia que inyecta la sesión de la base de datos
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+# Endpoint para obtener todos los registros
 @router.get("", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(Registros).all()
 
 
+# Endpoint para obtener un registro específico por ID
 @router.get("/{registro_id}", status_code=status.HTTP_200_OK)
 async def read_registro(db: db_dependency, registro_id: int = Path(gt=0)):
+    # Filtra el registro por ID
     registro_model = db.query(Registros).filter(
         Registros.id == registro_id).first()
 
+    # Si se encuentra el registro, lo retorna
     if registro_model is not None:
         return registro_model
+
+    # Si no se encuentra, retorna un error 404
     raise HTTPException(status_code=404, detail='Registro not found.')
 
 
+# Endpoint para obtener los detalles de un registro, incluyendo relaciones con otras tablas (persona y materia)
 @router.get("/detalle/{registro_id}", status_code=status.HTTP_200_OK)
 async def read_registro_detalle(db: db_dependency, registro_id: int = Path(gt=0)):
+    # Utiliza joinedload para cargar relaciones de persona y materia en la misma consulta
     registro_model = db.query(Registros).options(
-        joinedload(Registros.persona), 
+        joinedload(Registros.persona),
         joinedload(Registros.materia)
     ).filter(Registros.id == registro_id).first()
 
+    # Si el registro no se encuentra, devuelve un error 404
     if registro_model is None:
         raise HTTPException(status_code=404, detail='Registro not found.')
 
+    # Retorna el registro junto con los detalles de las relaciones
     return registro_model
 
 
+# Endpoint para crear un nuevo registro
 @router.post("", status_code=status.HTTP_201_CREATED)
 async def create_registro(db: db_dependency,
                           registro_request: RegistroRequest):
+    # Creamos una instancia del modelo Registros con los datos recibidos
     registro_model = Registros(**registro_request.model_dump())
 
     try:
+        # Agrega el nuevo registro a la base de datos
         db.add(registro_model)
         db.commit()
         return registro_model
-    except IntegrityError as e:
-        db.rollback()
 
+    except IntegrityError as e:
+        db.rollback()  # En caso de error de integridad, se revierte la transacción
+
+        # Verifica si el ID de la materia existe
         materia_id = db.query(Materias).filter(
             Materias.id == registro_request.materia_id
             ).first()
         if materia_id is None:
             raise HTTPException(status_code=422, detail='materia_id does not exist.')
 
+        # Verifica si el ID de la persona existe
         persona_id = db.query(Personas).filter(
             Personas.id == registro_request.persona_id
             ).first()
@@ -85,17 +104,21 @@ async def create_registro(db: db_dependency,
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
 
+# Endpoint para actualizar un registro existente
 @router.put("/{registro_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def update_registro(db: db_dependency,
                           registro_request: RegistroRequest,
                           registro_id: int = Path(gt=0)):
+    # Busca el registro por ID
     registro_model = db.query(Registros).filter(
         Registros.id == registro_id
         ).first()
 
+    # Si no se encuentra, retorna un error 404
     if registro_model is None:
         raise HTTPException(status_code=404, detail='Registro not found.')
 
+    # Actualiza los campos del registro con los datos de la request
     registro_model.calificacion_final = registro_request.calificacion_final
     registro_model.fecha_inicio_cursado = registro_request.fecha_inicio_cursado
     registro_model.fecha_fin_cursado = registro_request.fecha_fin_cursado
@@ -106,10 +129,12 @@ async def update_registro(db: db_dependency,
     try:
         db.add(registro_model)
         db.commit()
-        return registro_model
+        return registro_model  # Retorna el registro actualizado
+
     except IntegrityError as e:
         db.rollback()
 
+        # Validaciones de existencia de IDs de persona y materia, similar a la creación
         materia_id = db.query(Materias).filter(
             Materias.id == registro_request.materia_id
             ).first()
@@ -129,15 +154,18 @@ async def update_registro(db: db_dependency,
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
 
+# Endpoint para eliminar un registro existente
 @router.delete("/{registro_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_registro(db: db_dependency, registro_id: int = Path(gt=0)):
+    # Busca el registro por ID
     registro_model = db.query(Registros).filter(
         Registros.id == registro_id
         ).first()
 
+    # Si el registro no se encuentra, retorna un error 404
     if registro_model is None:
         raise HTTPException(status_code=404, detail='Registro not found.')
 
+    # Elimina el registro de la base de datos y confirma la transaction
     db.delete(registro_model)
-
     db.commit()
